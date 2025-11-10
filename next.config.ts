5e03e3477e5620dff9e5892ff98e88cb529a068c
sasith duleepa <duleepajayathissa@gmail.com>
2025-11-10 09:30:39 +0530

    home

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'https://flowbite.s3.amazonaws.com', // Replace with your image host domain
+        port: '',
+        pathname: '/blocks/marketing-ui/avatars/**', // Optional: restrict to a specific path
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
+
+module.exports = nextConfig;
---
+++ src/app/home component/cooking/page.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image"
+import image2 from "./../../../../public/images/4.png"
+import Link from "next/link"
+export default function HomeCooking() {
+  return (
+    <div className="text-center my-20">
+      <h1 className="font-qwitcher  mb-4 xl:mb-6 text-foreground text-7xl xl:text-9xl">
+        L<span className="text-primary font-semibold">earn to </span>
+        C<span className="text-primary font-semibold">ook </span>
+        T<span className="text-primary font-semibold">raditional </span>
+        S<span className="text-primary font-semibold">ri </span>
+        L<span className="text-primary font-semibold">ankan </span>
+        M<span className="text-primary font-semibold">eals</span>
+      </h1>
+
+      
+
+
+      <div className="flex align-top content-between w-[100%] gap-20">
+        <div className="w-[40%]">
+          <p className="text-muted-foreground text-sm md:text-xl font-medium max-w-3xl mx-auto text-pretty leading-relaxed [text-shadow:_0_4px_14px_rgba(0,0,0,0.35)]">
+        <span className="font-semibold">Join our hands-on cooking class and discover the secrets of Sri Lankan cuisine - </span>
+        from spicy curries to crispy hoppers. Cook, taste, and laugh together in a warm, home-style kitchen.
+      </p>
+
+      <Link href="/cooking-class">Explore More</Link>
+        </div>
+        <div className="w-[40%] flex flex-col">
+          <Image className=" rounded-2xl" src={image2} 
+            // width={100} height={100}
+            alt="profile picture"/>
+            <Image className="rounded-2xl ml-[30%] mt-[-10%]" src={image2} 
+            // width={100} height={100}
+            alt="profile picture"/>
+        </div>
+      </div>
+
+
+      
+    </div>
+  )
+}
+
+
